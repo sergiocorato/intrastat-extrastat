4942706f736f06a5fd9af2c54d7abd6ddb27762a
sergiocorato <sergiocorato@gmail.com>
2021-12-12 00:01:19 +0100

    [FIX] char intrastat

--- product_harmonized_system/models/product_template.py
+++ product_harmonized_system/models/product_template.py
@@ -11,8 +11,7 @@ from openerp import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    intrastat_hs_code_id = fields.Many2one(
-        comodel_name='hs.code',
+    intrastat_hs_code = fields.Char(
         string="Intrastat HS Code")
     hs_code_id = fields.Many2one(
         'hs.code', string='H.S. Code',
